6e5a9c991ca0c5711bf58a2e9e6a4377b9fa89aa
heraces <david.ballesta@hotmail.com>
2020-04-20 13:42:37 +0200

    bloques mas o menos

--- js/play.js
+++ js/play.js
@@ -1,9 +1,18 @@
+
+const NUM_BLOCKS = 7;
+let haveToPutNoBlock = true;
+let blockX = -30;
+let blockY = 50;
+let blockPos = 1;
+
 let player;
 let playerAcceleration;
 let playerVelocity;
 let playerName;
 
 let levelConfig;
+
+let blocks;
 
 let playState = {
     preload: preloadPlay,
@@ -14,18 +23,22 @@ let playState = {
 function preloadPlay(){
     game.load.spritesheet('player','assets/imgs/Slime/Slime 16x16.png', 16, 16);
     game.load.spritesheet('enviroment','assets/imgs/Enviroment/Assets/Assets.png', 16, 16);
+    game.load.image("bloque", "assets/imgs/bloque.png");
 }
 
 function createPlay(){
     //levelConfig = JSON.parse(game.cache.getText('level')); //Ni idea como va esto julio
     createPlayer();
     createKeyControls();
+    createBlock();
     game.canvas.oncontextmenu = function (e) { e.preventDefault(); }
+
 
 }
 
 function updatePlay(){
     managePlayerVelocity();
+    manageBlockMovement();
 }
 
 function createPlayer(){
@@ -38,9 +51,68 @@ function createPlayer(){
     
 }
 
+
+function createBlock(){
+    blocks = game.add.group();
+    blocks.enableBody = true;
+    blocks.createMultiple(NUM_BLOCKS, 'bloque');
+    blocks.forEach(setUpBlock, this);
+}
+
+function setUpBlock(){
+    let item = blocks.getFirstExists(false);
+    if (item) {
+        let rand = Math.random();
+        if(haveToPutNoBlock && rand<blockPos/NUM_BLOCKS){
+            delete(item);
+            haveToPutNoBlock = false;
+        }
+        else{
+            item.reset(blockX, blockY);
+        }
+        blockX+= 60;
+        blockPos++;
+    }
+        
+}
+
+
 function managePlayerVelocity(){
 
 }
+
+function manageBlockMovement(){
+   if(cursorLeft.justDown){
+        blocks.forEach(moveLeftBlock);
+    }
+    if(cursorRigh.justDown){
+        blocks.forEach(moveRightBlock);      
+    }
+}
+
+function moveLeftBlock(block){
+   block.body.x -= 60;
+   if(block.body.x < -30){
+        block.body.x = 400 + block.body.x;
+   }/*
+   else if(block.body.x < 400 && block.body.x > 370){
+       let newBlock = game.add.sprite(block.body.x+60, block.body.y, "bloque");
+       blocks.add(newBlock);
+   }*/
+
+}
+
+function moveRightBlock(block){
+    block.body.x += 60;
+    if(block.body.x > 430){
+        delete(block);
+    }
+   /* else if(block.body.x > 370){
+        let newBlock = game.add.sprite(400-block.body.x, block.body.y, "bloque");
+        blocks.add(newBlock);
+    }*/
+}
+
 
 function createKeyControls(){
     cursorLeft = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
