47f613bd8a98d4fef878ac50dee4fc522c8e3209
Dominik Konrad Kwiek <randomgameplayer1234@gmail.com>
2020-07-03 16:50:38 +0200

    Ya no hay explosión al principio

--- js/start.js
+++ js/start.js
@@ -14,7 +14,6 @@ let backgound, mine_text, detector_text;
 
 let firstStart;
 let textAnimationTime;
-let explosionAtStart;
 let currentLevel;
 
 //Estilo genérico
@@ -34,11 +33,6 @@ function cargaAssets()
     game.load.image('Mine_text','assets/imgs/New UI/PNG/Mine_Text.png');
     game.load.image('Detector_text','assets/imgs/New UI/PNG/Detector_Text.png');
 
-    game.load.spritesheet('explosion','assets/imgs/Mina/Explosion_SpriteSheet.png',210,210,58);
-    game.load.image('pedacito1','assets/imgs/New Player/Pedacitos/Pedacito1.png');
-    game.load.image('pedacito2','assets/imgs/New Player/Pedacitos/Pedacito2.png');
-    game.load.image('pedacito3','assets/imgs/New Player/Pedacitos/Pedacito3.png');
-    game.load.image('pedacito4','assets/imgs/New Player/Pedacitos/Pedacito4.png');
 }
 
 function muestraPantalla()
@@ -50,16 +44,6 @@ function muestraPantalla()
     background.scale.setTo(0.6,0.6);
 
     if(firstStart){
-/*
-        mine_text = game.add.image(0,0,'Mine_text');
-        mine_text.scale.setTo(0.7,0.7);
-        mine_text.centerX = 200;
-        mine_text.y = 50;
-        detector_text = game.add.image(0,0,'Detector_text');
-        detector_text.scale.setTo(0.7,0.7);
-        detector_text.centerX = 200;
-        detector_text.y = 130;
-*/    
         
         mine_text = game.add.image(0,0,'Mine_text');
         mine_text.scale.setTo(0.7,0.7);
@@ -72,8 +56,6 @@ function muestraPantalla()
 
         game.add.tween(mine_text).to({centerX:200},textAnimationTime,"Linear",true);
         game.add.tween(detector_text).to({centerX:200},textAnimationTime,"Linear",true);
-
-        game.time.events.add(textAnimationTime, playExplosion, this);
 
         btnSelectLevel = game.add.button(39, 250, 'botonSelectLevel', selectLevelPressed);
         btnSelectLevel.alpha = 0;
@@ -96,7 +78,6 @@ function muestraPantalla()
         btnSelectNameAnim.onComplete.add(function(){btnSelectName.inputEnabled = true});
         btnAboutAnim.onComplete.add(function(){btnAbout.inputEnabled = true});
         btnInstrucionsAnim.onComplete.add(function(){btnInstrucions.inputEnabled = true});
-
 
         firstStart = false;
     }
@@ -121,20 +102,6 @@ function muestraPantalla()
             btnSelectLevel.inputEnabled = false;
         }
     }
-}
-
-function playExplosion(){
-    let startPartsEmitter = game.add.emitter(200, 100, 100);
-    startPartsEmitter.makeParticles(['pedacito1','pedacito2','pedacito3','pedacito4']);
-    startPartsEmitter.gravity = 40;
-    startPartsEmitter.minParticleScale = 0.15;
-    startPartsEmitter.maxParticleScale = 0.20;
-    startPartsEmitter.setAlpha(1, 0, 2000);
-    startPartsEmitter.start(true, 2000, null, 12);
-
-    explosionAtStart = game.add.sprite(100,5,'explosion');
-    explosionAtStart.animations.add('bum');
-    explosionAtStart.animations.play('bum',30,false,true);
 }
 
 // MÉTODOS DE LOS BOTONES --------------------------
